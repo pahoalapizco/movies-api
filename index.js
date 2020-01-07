b1238d4cabce6640b22e4acf7339e8f3366726d2
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-01-07 11:27:21 -0700

    Implementacion de boom para mensajes de error, se incluye el manejador del error 404

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const { config } = require('./config/');
 const moviesApi = require('./routes/movies');
-const { logError, errorHandler } = require('./utils/middleware/errorHandler');
+const {
+  logError,
+  errorHandler,
+  wrapError
+} = require('./utils/middleware/errorHandler');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 const app = express();
 
@@ -10,8 +15,13 @@ app.use(express.json());
 
 moviesApi(app);
 
+// catch 404 error
+app.use(notFoundHandler);
+
 // Los middleware's deben ir despues de las rutas de api
+// ERRORS!
 app.use(logError);
+app.use(wrapError);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
--- utils/middleware/errorHandler.js
+++ utils/middleware/errorHandler.js
@@ -1,8 +1,9 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config/');
 
 function withErrorStack(err, stack) {
   if (config.dev) {
-    return { err, stack };
+    return { ...err, stack };
   }
 
   return error;
@@ -13,12 +14,23 @@ function logError(err, req, res, next) {
   next(err);
 }
 
+function wrapError(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err));
+  }
+
+  next(err);
+}
+
 function errorHandler(err, req, res, next) {
-  res.status(err.status || 500).json(withErrorStack(err.message, err.stack));
+  const {
+    output: { statusCode, payload }
+  } = err;
+  res.status(statusCode || 500).json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
-  withErrorStack,
+  wrapError,
   logError,
   errorHandler
-}
+};
